0e972bb0ed68ddbf5d404732d7bc30945f38965e
Vinay Krishna <vinaykrishna1989@gmail.com>
2022-03-31 13:51:00 -0400

    replacing service bean definition with annotation

--- src/main/java/info/vinkrish/AppConfig.java
+++ src/main/java/info/vinkrish/AppConfig.java
@@ -1,19 +1,11 @@
 package info.vinkrish;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import info.vinkrish.service.CustomerService;
-import info.vinkrish.service.CustomerServiceImpl;
 
 @Configuration
 @ComponentScan({"info.vinkrish"})
 public class AppConfig {
 
-	@Bean(name = "customerService")
-	public CustomerService getCustomerService() {
-		CustomerServiceImpl service =  new CustomerServiceImpl();
-		return service;
-	}
 
 }
--- src/main/java/info/vinkrish/service/CustomerServiceImpl.java
+++ src/main/java/info/vinkrish/service/CustomerServiceImpl.java
@@ -3,10 +3,12 @@ package info.vinkrish.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import info.vinkrish.model.Customer;
 import info.vinkrish.repository.CustomerRepository;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 	
 	private CustomerRepository customerRepository;
