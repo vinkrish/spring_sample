5fbd303273bf8105d434c3853ffc9104893d6201
Vinay Krishna <vinaykrishna1989@gmail.com>
2022-03-31 14:11:17 -0400

    singleton or prototype class type defined with scope annotation

--- src/main/java/info/vinkrish/service/CustomerServiceImpl.java
+++ src/main/java/info/vinkrish/service/CustomerServiceImpl.java
@@ -3,12 +3,15 @@ package info.vinkrish.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import info.vinkrish.model.Customer;
 import info.vinkrish.repository.CustomerRepository;
 
 @Service("customerService")
+//@Scope("prototype")
+@Scope("singleton")
 public class CustomerServiceImpl implements CustomerService {
 	
 	private CustomerRepository customerRepository;
